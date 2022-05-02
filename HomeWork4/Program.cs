2aa9fc6ffc82fcdba109bca85e62adcbd023d2be
SteelPlated <mix.singatulin@yandex.ru>
2022-05-02 16:07:05 +0500

    Задача4 выполнена

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Найти максимальное из трех чисел 
+
+Console.Write("Enter number 1 -> ");
+int num1 =int.Parse(Console.ReadLine());
+Console.Write("Enter number 2 -> ");
+int num2 =int.Parse(Console.ReadLine());
+Console.Write("Enter number 3 -> ");
+int num3 =int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max ) max = num1;
+if (num2 > max ) max = num2;
+if (num3 > max ) max = num3;
+
+Console.Write("max= ");
+Console.WriteLine(max);
