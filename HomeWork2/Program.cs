c0c91b6ea462fcca442dcb51498c673f19a5e28d
SteelPlated <mix.singatulin@yandex.ru>
2022-05-02 20:33:15 +0500

    переделал задачу 2

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -5,14 +5,21 @@ int num1 =int.Parse(Console.ReadLine());
 Console.Write("Enter number 2 -> ");
 int num2 =int.Parse(Console.ReadLine());
 
-int max = num1; 
+int max=num1; 
 int min=num2;
 
-if (num1 > max ) max = num1; min=num2;
-if (num2 > max ) max = num2; min=num2;
-
-
-Console.Write("max= ");
-Console.WriteLine(max);
-Console.Write("min= ");
-Console.WriteLine(min);
+if (num1 > num2) {
+    max = num1; 
+    min=num2;
+    Console.WriteLine($"max= {max}");
+    Console.WriteLine($"min= {min}");
+    }
+else
+    if (num2 > num1) {
+        max = num2; 
+        min=num1;
+        Console.WriteLine($"max= {max}");
+        Console.WriteLine($"min= {min}");
+    }
+    else 
+    Console.WriteLine("Числа равны");
