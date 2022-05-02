369131f7464af294021ce90029927de677393bd5
SteelPlated <mix.singatulin@yandex.ru>
2022-05-02 22:04:42 +0500

    Выполнил задание 8

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,10 @@
+﻿// 8. Показать все четные числа от 1 до N.
+
+Console.Write("Enter N -> ");
+int N =int.Parse(Console.ReadLine());
+int i=2;
+while ( i<= N)
+    {
+        Console.Write(i + " ");
+        i=i+2;
+    }
