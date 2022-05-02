18830917e2323c50787b59c94bb805414fa31932
SteelPlated <mix.singatulin@yandex.ru>
2022-05-02 20:34:33 +0500

    задача6 выполнена

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6. Определить четное чилсло или нет.
+
+Console.Write("Enter number 1 -> ");
+int num1 =int.Parse(Console.ReadLine());
+
+if (num1%2==0) 
+{
+Console.Write("Число четное");
+}
+else 
+Console.Write("Число НЕчетное");
