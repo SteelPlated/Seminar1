fc3ff2dd1b66ce5384267ef7f33a4e916c1422d5
SteelPlated <mix.singatulin@yandex.ru>
2022-05-02 16:33:55 +0500

    Задача2 выполнена

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2. Найти максимальное из двух чисел 
+
+Console.Write("Enter number 1 -> ");
+int num1 =int.Parse(Console.ReadLine());
+Console.Write("Enter number 2 -> ");
+int num2 =int.Parse(Console.ReadLine());
+
+int max = num1; 
+int min=num2;
+
+if (num1 > max ) max = num1; min=num2;
+if (num2 > max ) max = num2; min=num2;
+
+
+Console.Write("max= ");
+Console.WriteLine(max);
+Console.Write("min= ");
+Console.WriteLine(min);
